ae1179fee37d95b49f017d3a8a47edf630bf4740
itrollyou <pguerrero002@ikasle.ehu.eus>
2022-05-25 17:18:23 +0200

    Final sección 5

---
+++ src/app/dbz/services/dbz.services.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { Personaje } from "../interfaces/dbz.interface";
+
+@Injectable()
+export class DbzService {
+
+    private _personajes: Personaje[] = [
+        {
+            nombre: "Goku",
+            poder: 15000
+        },
+        {
+            nombre: "Vegeta",
+            poder: 8000
+        }
+    ]
+
+    get personajes(): Personaje[] {
+        return [... this._personajes];
+    }
+
+    agregarPersonajes(personaje: Personaje) {
+        this._personajes.push(personaje);
+    }
+
+    constructor() {
+        console.log("Servicio inicializado");
+    }
+}
